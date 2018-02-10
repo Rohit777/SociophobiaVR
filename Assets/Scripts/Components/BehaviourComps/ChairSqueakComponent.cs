457e1153fdf16bd784ad1218ecbe7aac429d388f
Shreyas Iyer <Shreyas.Srinivasan@iiitb.org>
2018-02-11 04:37:08 +0530

    Started working with the chair squeaking component

---
+++ Assets/Scripts/Components/BehaviourComps/ChairSqueakComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairSqueakComponent : AbstractComponent {
+	private float probabilityFactor;
+	private Transform chairObject;
+	private AudioSource audioSource;
+
+	[SerializeField] private float timeProbability;
+	[SerializeField] private float rotationSpeed;
+	[SerializeField] private AudioClip chairSqueakSound;
+
+	private void Start () {
+		audioSource = GetComponent<AudioSource> ();
+		chairObject = transform.parent;
+		probabilityFactor = Mathf.Exp (-3.0f);
+		timeProbability = Mathf.Clamp (Mathf.Exp (probabilityFactor * Time.time), 0f, 100f);
+	}
+
+	IEnumerator SqueakAnimation () {
+		while (true) {
+			chairObject.Rotate (new Vector3 (0f, Mathf.Sin (Time.time) * rotationSpeed, 0f));
+			yield return null;
+		}
+	}
+}
