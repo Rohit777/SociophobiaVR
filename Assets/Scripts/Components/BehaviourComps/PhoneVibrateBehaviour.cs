44dfc43efe9364b9b4579223451733a3e344b087
Shreyas Iyer <Shreyas.Srinivasan@iiitb.org>
2018-02-21 05:19:31 +0530

    Added Microphone recording utils, and a new PhoneVibration behaviour.

--- Assets/Scripts/Components/BehaviourComps/PhoneVibrateBehaviour.cs
+++ Assets/Scripts/Components/BehaviourComps/PhoneVibrateBehaviour.cs
@@ -2,19 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChairSqueakComponent : NPCComponent {
-	private Transform chairObject;
+public class PhoneVibrateBehaviour : NPCComponent {
 	private AudioSource audioSource;
 	private float timeProbability;
 
 	[SerializeField] private float probabilityFactor;
-	[SerializeField] private float rotationSpeed;
-	[SerializeField] private AudioClip chairSqueakSound;
+	[SerializeField] private AudioClip vibrateSound;
 
 	private void Start () {
 		audioSource = GetComponent<AudioSource> ();
-		audioSource.clip = chairSqueakSound;
-		chairObject = transform.parent;
+		audioSource.clip = vibrateSound;
 	}
 
 	public override IEnumerator NPCAction () {
@@ -26,8 +23,6 @@ public class ChairSqueakComponent : NPCComponent {
 			if (timeProbability >= 90f && timeProbability <= 195f) {
 				if (!audioSource.isPlaying)
 					audioSource.Play ();
-	
-				chairObject.Rotate (new Vector3 (0f, 0.4f * Mathf.Sin (Time.time) * rotationSpeed, 0f));
 			} else {
 				audioSource.Stop ();
 			}
---
+++ Assets/Scripts/Systems/SoundSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSystem : MonoBehaviour {
+
+	[SerializeField] private int sessionLength;
+	[SerializeField] private int recordingFrequency;
+	[SerializeField] private GameObject player;
+
+	private GvrAudioSource audioSource;
+
+	private void Start () {
+		audioSource = player.GetComponent<GvrAudioSource> (); 
+		audioSource.clip = Microphone.Start("", false, sessionLength, recordingFrequency);
+	}
+
+	public void PlayRecordedAudio () {
+		audioSource.Play ();
+	}
+}
