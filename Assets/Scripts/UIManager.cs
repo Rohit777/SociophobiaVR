0c8cfbc18d10f9fb8e2d86ee479efacac0153257
Shreyas Iyer <Shreyas.Srinivasan@iiitb.org>
2018-04-04 10:46:13 +0530

    Added a basic controller UI.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour {
 	public static UIManager instance;
 
 	[SerializeField] private NPCSystem npcSystem;
+	[SerializeField] private Transform compListParent;
+
+	private GameObject npcComponentListPrefab;
+	private GameObject componentListItemPrefab;
+	private GameObject npcGameObjectName;
+	private GameObject compListItemParent;
 
 	void Start () {
 		if (instance == null) {
@@ -13,11 +20,35 @@ public class UIManager : MonoBehaviour {
 		} else {
 			Destroy (gameObject);
 		}
+		npcComponentListPrefab = (GameObject) Resources.Load ("ComponentList");
+		componentListItemPrefab = (GameObject) Resources.Load ("ComponentButton");
+		npcGameObjectName = (GameObject)Resources.Load ("GameObjectName");
+		ControllerUISetup ();
 	}
 	
 	public void StopSessionAction () {
 		//Debug.Log ("Session stopped");
 		npcSystem.StopAllNPCAction ();
 		EventManager.closeSession ();
+	}
+
+	void ControllerUISetup () {
+		foreach (GameObject NPCObject in EntityManager.getObjectsOfType<NPCComponent>()) {
+			GameObject _newList = Instantiate (npcComponentListPrefab);
+			_newList.transform.position = Vector3.zero;
+			_newList.transform.rotation = Quaternion.identity;
+			_newList.transform.SetParent (compListParent);
+
+			NPCComponent[] npcComps = NPCObject.GetComponents<NPCComponent> ();
+			compListItemParent = _newList.transform.GetChild (0).GetChild (0).gameObject;
+			GameObject _nameObj = Instantiate (npcGameObjectName);
+			_nameObj.GetComponent<Text> ().text = NPCObject.name;
+			_nameObj.transform.SetParent (compListItemParent.transform);
+			foreach (NPCComponent npcComp in npcComps) {
+				GameObject _newListItem = Instantiate (componentListItemPrefab);
+				_newListItem.transform.SetParent (compListItemParent.transform);
+				_newListItem.GetComponent<ComponentListItem> ().Setup (npcComp);
+			}
+		}
 	}
 }
